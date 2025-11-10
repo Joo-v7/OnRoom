375dbf533d730efce7a01efd975c061c399cad50
joo <helloworld.v7@gmail.com>
2025-11-11 07:54:53 +0900

    부트스트랩 추가

---
+++ src/main/java/egovframework/home/pg/BootStrapController.java
@@ -0,0 +1,28 @@
+package egovframework.home.pg;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Slf4j
+@Controller
+@RequestMapping("/bootstrap")
+public class BootStrapController {
+
+    @RequestMapping("/index.do")
+    public String index() {
+        return "example/bootstrap/index";
+    }
+
+    @RequestMapping("/sidebar.do")
+    public String sidebar() {
+        return "example/bootstrap/sidebar";
+    }
+
+    @GetMapping("/hello.do")
+    public String hello() {
+        return "example/bootstrap/helloworld";
+    }
+}
